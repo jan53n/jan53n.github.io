31b086265f6ac58505c48be328e4bd1b8f3c2605
Jansen <jan53n@outlook.in>
2025-02-09 16:35:28 +0530

    build

---
+++ js/colpref.js
@@ -0,0 +1,43 @@
+addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("colpref");
+    const ls = localStorage;
+    const pref = ls.getItem("colpref");
+    const cl = document.body.classList;
+
+    function setAuto() {
+        const isDark = matchMedia && matchMedia('(prefers-color-scheme: dark)').matches;
+        ls.removeItem("colpref");
+
+        if (!cl.contains("auto")) {
+            cl.add("auto");
+        }
+
+        cl.remove("dark", "light");
+        cl.add(
+            isDark
+                ? "dark"
+                : "light");
+    }
+
+    if (pref === "0") {
+        cl.add("light");
+    } else if (pref === "1") {
+        cl.add("dark");
+    } else {
+        setAuto();
+    }
+
+    btn.addEventListener("click", () => {
+        cl.remove("auto");
+
+        if (cl.contains("dark")) {
+            cl.replace("dark", "light");
+            ls.setItem("colpref", "0");
+        } else {
+            cl.replace("light", "dark");
+            ls.setItem("colpref", "1");
+        }
+    });
+
+    btn.addEventListener("dblclick", setAuto);
+});
